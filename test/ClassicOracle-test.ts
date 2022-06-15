cd86c30717a9da3b78c35874c9098bfdff45ac58
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2022-06-15 16:48:40 +0200

    Update to default account & reporter account

--- test/ClassicOracle-test.ts
+++ test/ClassicOracle-test.ts
@@ -1,32 +1,31 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { Signer } from "ethers";
-import { ClassicOracle, ClassicOracle__factory } from "../typechain";
+import { ClassicOracle } from "../typechain";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 
 describe("ClassicOracle", function () {
-    let accountOne: SignerWithAddress
-    let accountTwo: SignerWithAddress
+    let defaultAccount: SignerWithAddress
+    let reporterAccount: SignerWithAddress
     let classicOracle: ClassicOracle
     const oracleId = ethers.utils.keccak256(new TextEncoder().encode("oracleId"))
     console.log("oracleId: " + oracleId);
 
     beforeEach("Fresh contract & accounts", async () => {
-        [accountOne, accountTwo] = await ethers.getSigners();
+        [defaultAccount, reporterAccount] = await ethers.getSigners();
 
         const ClassicOracleFactory = await ethers.getContractFactory("ClassicOracle")
-        classicOracle = await ClassicOracleFactory.deploy(accountTwo.address)
+        classicOracle = await ClassicOracleFactory.deploy(reporterAccount.address)
     });
 
     it('should construct with owner address as authorized reporter when constructor argument is 0x0', async () => {
         const ClassicOracleFactory = await ethers.getContractFactory("ClassicOracle")
         classicOracle = await ClassicOracleFactory.deploy(ethers.constants.AddressZero)
-        expect(await classicOracle.authorizedReporter()).equal(accountOne.address)
+        expect(await classicOracle.authorizedReporter()).equal(defaultAccount.address)
     });
 
     it('should construct with custom address as authorized reporter', async () => {
-        expect(await classicOracle.authorizedReporter()).equal(accountTwo.address)
+        expect(await classicOracle.authorizedReporter()).equal(reporterAccount.address)
     });
 
     it('should not receive since reporter not authorized', async () => {
@@ -34,7 +33,7 @@ describe("ClassicOracle", function () {
         const value: string = 'abcd';
         const callback: string = buildCallback(oracleId, date, ethers.utils.defaultAbiCoder.encode(['string'], [value]));
 
-        await expect(classicOracle.connect(accountOne).receiveResult(oracleId, callback))
+        await expect(classicOracle.connect(defaultAccount).receiveResult(oracleId, callback))
             .to.be.revertedWith("Reporter is not authorized");
     });
 
@@ -43,7 +42,7 @@ describe("ClassicOracle", function () {
         const value: string = 'abcd';
         const callback: string = buildCallback(oracleId, date, ethers.utils.defaultAbiCoder.encode(['string'], [value]));
 
-        await expect(classicOracle.connect(accountTwo).receiveResult(oracleId, callback))
+        await expect(classicOracle.connect(reporterAccount).receiveResult(oracleId, callback))
             .to.emit(classicOracle, 'ValueUpdated');
 
         const { date: foundDate, stringValue: foundValue } = await classicOracle.getString(oracleId);
@@ -55,7 +54,7 @@ describe("ClassicOracle", function () {
         const date: BigInt = generateDate();
         const value: Number = 123456789;
         const callback: string = buildCallback(oracleId, date, ethers.utils.defaultAbiCoder.encode(['int'], [value]));
-        await classicOracle.connect(accountTwo).receiveResult(oracleId, callback)
+        await classicOracle.connect(reporterAccount).receiveResult(oracleId, callback)
 
         const { date: foundDate, intValue: foundValue } = await classicOracle.getInt(oracleId);
         expect(date).equal(foundDate);
@@ -66,7 +65,7 @@ describe("ClassicOracle", function () {
         const date: BigInt = generateDate();
         const value: boolean = true;
         const callback: string = buildCallback(oracleId, date, ethers.utils.defaultAbiCoder.encode(['bool'], [value]));
-        await classicOracle.connect(accountTwo).receiveResult(oracleId, callback)
+        await classicOracle.connect(reporterAccount).receiveResult(oracleId, callback)
 
         const { date: foundDate, boolValue: foundValue } = await classicOracle.getBool(oracleId);
         expect(date).equal(foundDate);
@@ -77,7 +76,7 @@ describe("ClassicOracle", function () {
         const date: BigInt = generateDate();
         const value: string = "0xaaaabbbbccccddddeeeeffff0000111122223333444455556666777788889999"
         const callback: string = buildCallback(oracleId, date, ethers.utils.defaultAbiCoder.encode(['bytes32'], [value]));
-        await classicOracle.connect(accountTwo).receiveResult(oracleId, callback)
+        await classicOracle.connect(reporterAccount).receiveResult(oracleId, callback)
 
         const { date: foundDate, bytesValue: foundValue } = await classicOracle.getRaw(oracleId);
         expect(date).equal(foundDate);
