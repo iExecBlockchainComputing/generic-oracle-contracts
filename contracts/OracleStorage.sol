0154ae74c67bcd42c4c9bf8c3bcef210360c5fae
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2022-10-12 15:39:16 +0200

    Externalize storage from ClassicOracle

---
+++ contracts/OracleStorage.sol
@@ -0,0 +1,88 @@
+// SPDX-License-Identifier: Apache-2.0
+
+/******************************************************************************
+ * Copyright 2022 IEXEC BLOCKCHAIN TECH                                       *
+ *                                                                            *
+ * Licensed under the Apache License, Version 2.0 (the "License");            *
+ * you may not use this file except in compliance with the License.           *
+ * You may obtain a copy of the License at                                    *
+ *                                                                            *
+ *     http://www.apache.org/licenses/LICENSE-2.0                             *
+ *                                                                            *
+ * Unless required by applicable law or agreed to in writing, software        *
+ * distributed under the License is distributed on an "AS IS" BASIS,          *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   *
+ * See the License for the specific language governing permissions and        *
+ * limitations under the License.                                             *
+ ******************************************************************************/
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract OracleStorage {
+
+    // Data storage
+    struct TimedRawValue {
+        bytes value;
+        uint256 date;
+    }
+
+    mapping(bytes32 => TimedRawValue) public values;
+
+    // Event
+    event ValueUpdated(
+        bytes32 indexed id,
+        bytes32 indexed oracleCallID,
+        uint256 date,
+        bytes value
+    );
+
+    // Update value
+    function updateValue(bytes32 id, bytes32 callId, uint256 date, bytes memory value)
+        internal
+        returns (bool)
+    {
+        values[id].date = date; //What if date is older?
+        values[id].value = value;
+        emit ValueUpdated(id, callId, date, value);
+        return true;
+    }
+
+    // Read value
+    function getString(bytes32 _oracleId)
+        public
+        view
+        returns (string memory stringValue, uint256 date)
+    {
+        bytes memory value = values[_oracleId].value;
+        return (abi.decode(value, (string)), values[_oracleId].date);
+    }
+
+    function getRaw(bytes32 _oracleId)
+        public
+        view
+        returns (bytes memory bytesValue, uint256 date)
+    {
+        bytes memory value = values[_oracleId].value;
+        return (value, values[_oracleId].date);
+    }
+
+    function getInt(bytes32 _oracleId)
+        public
+        view
+        returns (int256 intValue, uint256 date)
+    {
+        bytes memory value = values[_oracleId].value;
+        return (abi.decode(value, (int256)), values[_oracleId].date);
+    }
+
+    function getBool(bytes32 _oracleId)
+        public
+        view
+        returns (bool boolValue, uint256 date)
+    {
+        bytes memory value = values[_oracleId].value;
+        return (abi.decode(value, (bool)), values[_oracleId].date);
+    }
+}
