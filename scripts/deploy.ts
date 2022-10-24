cb6fe31efadaa6ef04a7139d07fdd70d6a23245d
Pierre Jeanjacquot <26487010+PierreJeanjacquot@users.noreply.github.com>
2022-10-24 14:45:01 +0200

    get authorizedReporter from env

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,15 +1,23 @@
 import { ethers } from "hardhat";
 
+const { AUTHORIZED_REPORTER } = process.env;
+
 async function main() {
-    const [deployer] = await ethers.getSigners();
-    console.log("Deploying contracts with the account:", deployer.address);
-    const authorizedReporter = '0x767A2D69D7278F200ae1F79a00Ac2CaE299dD784'
-    const SaltyForwarderFactory = await ethers.getContractFactory("SaltyForwarder")
-    const forwarder = await SaltyForwarderFactory.deploy()
-    console.log("SaltyForwarder deployed to:", forwarder.address);
-    const ClassicOracleFactory = await ethers.getContractFactory("ClassicOracle")
-    const classicOracle = await ClassicOracleFactory.deploy(authorizedReporter, forwarder.address)
-    console.log("ClassicOracle deployed to:", classicOracle.address);
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  const authorizedReporter =
+    AUTHORIZED_REPORTER || "0x767A2D69D7278F200ae1F79a00Ac2CaE299dD784";
+  const SaltyForwarderFactory = await ethers.getContractFactory(
+    "SaltyForwarder"
+  );
+  const forwarder = await SaltyForwarderFactory.deploy();
+  console.log("SaltyForwarder deployed to:", forwarder.address);
+  const ClassicOracleFactory = await ethers.getContractFactory("ClassicOracle");
+  const classicOracle = await ClassicOracleFactory.deploy(
+    authorizedReporter,
+    forwarder.address
+  );
+  console.log("ClassicOracle deployed to:", classicOracle.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
